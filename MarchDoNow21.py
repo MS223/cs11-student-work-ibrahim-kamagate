4ee126ff7aa6142b5c68495b3723781b3a90d1b1
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-03-21 08:19:43 -0400

    Create MarchDoNow21.py

---
+++ MarchDoNow21.py
@@ -0,0 +1,9 @@
+import random
+random.randint(0, 3)
+random.randint(0, 3)
+print(random.randint(0, 3))
+print(random.randint(0, 3))
+print(random.randint(0, 3))
+#randit limits how far the numbers code go
+#0, 3 is everything between 0 to3 that number that the randit can choose from
+#when i switch the numbers I have to put the small number first them it will pck any number betwwen those two numbers
