47b97615895d38683544d902065d32cbd24036fc
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-03-28 08:08:08 -0400

    Update Fizzbuzz.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,10 +1,10 @@
 
 for groot in range (1,101):
-    if groot % 3 ==0:
-        print "fizz"
+    if groot % 5==0 + groot % 3==0:
+        print "fizzbuzz"
     elif groot % 5 ==0:
         print "buzz"
-    elif groot % 5==0 + groot % 3==0:
-        print "fizzbuzz"
+    elif groot % 3 ==0:
+        print "fizz"
     else:
         print groot
