8420112a4ca64f1123a7ac0dc5d5729f56dc1b72
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-03-29 08:36:38 -0400

    Create countdownV2.py

---
+++ countdownV2.py
@@ -0,0 +1,6 @@
+start = input("Give me a starting number?")#the user is going to give the number
+while start >0  in range(0,start):#it set the range from the number that the user put to 1
+    print start
+    start = start -1
+
+print "boom"
