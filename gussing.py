799f6278aaf2980112c965a75fb12a7809eabe0a
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-04-04 08:41:48 -0400

    Create gussing.py

---
+++ gussing.py
@@ -0,0 +1,9 @@
+import random
+random.randint(1, 10)
+number = input("I'm thinking of a number between 1 and 10")
+while number != random:
+    if number < random:
+        print "Higher"
+    else:
+        
+print(random.randint(0, 3))
