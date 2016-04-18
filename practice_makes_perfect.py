618b5d11edfd31cb4cd3bd8a6d8b377db5d2aca4
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-04-18 08:43:22 -0400

    Create practice_makes_perfect.py

---
+++ practice_makes_perfect.py
@@ -0,0 +1,13 @@
+number = input("Give me a random number")
+def is_even(input):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+def is_int(input):
+    if x == int(x):
+        return True
+    else:
+        return False
+is_int(6.7)
