63fd45ef34941334467eccf8902e052301ef0345
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-04-19 09:25:53 -0400

    Create scope.py

---
+++ scope.py
@@ -0,0 +1,42 @@
+# a = [1, 2, 4]
+# b = a #it had set b equal to a
+
+# input: a list of ints
+# output: an int
+# def update_list(a_list):#it replaced the thrid index in the list with yo
+#     b = a_list[4]
+#     b = 100
+# my_list = [1, 2, 3, 4, 5]
+# update_list(my_list)
+# print my_list
+
+# var_1 = "kittens"
+# var_2 = "cookies"
+# # input: a string
+# # output: a string
+# def my_function(my_favorite_things):
+#     song_lyrics = "rain drops on roses,"
+#     combined_song = song_lyrics + my_favorite_things
+#     return combined_song
+# # input: a string
+# # output: a string
+# def my_function_2(item, item2):
+#     full_lyrics = item + "on " + item2
+#     full_song = my_function(full_lyrics)
+#     return full_song
+# my_song = my_function_2(var_1, var_2)
+#
+# var_1 = 'cat'
+# var_2 = 'dog'
+#
+# def print_out_my_favorite(favorite_pet):
+#     if favorite_pet == var_1:
+#         print("My favorite pet is the cat.")
+#     elif favorite_pet == var_2:
+#         print("My favorite pet is the dog.")
+#     var_2 = "cat"
+#
+# print_out_my_favorite(var_1)
+# print(var_2)
+
+def my_num
