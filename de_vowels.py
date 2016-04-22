77e4fcf74ce900bf96fe533b8d00715cf7fb93c7
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-04-22 08:43:09 -0400

    Create de_vowels.py

---
+++ de_vowels.py
@@ -0,0 +1,5 @@
+def de_vowels(a_string):
+    for n in a_string:
+        if n != 'a' and n != 'e' and n != 'i' and n != 'o' and n != 'u':
+            print n
+de_vowels("laugh") #im stuck at trying to get everything on one line
