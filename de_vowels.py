b3760edba04c579028f0673cb1daad014a08a4d1
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-03 09:09:55 -0400

    Update de_vowels.py

--- de_vowels.py
+++ de_vowels.py
@@ -1,5 +1,5 @@
 vowels = ['a','e','i','o','u']
 h = raw_input("Give me a sentence").lower()
-for x in vowels:
-    h = h.replace(x,'')
+for x in vowels:#it looks for each letter in the sentence is in vowels
+    h = h.replace(x,'')#it replace all the letter that are vowel it replace them with nothing 
 print h
