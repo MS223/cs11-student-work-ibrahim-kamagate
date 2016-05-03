547bcb8f157bf9f146444b4d41105a71eac8ee2a
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-03 08:14:32 -0400

    Update max_value_compare_list.py

--- max_value_compare_list.py
+++ max_value_compare_list.py
@@ -1,8 +1,8 @@
 # number = input("Give me a radom number")
 # max_value= range(1, number +1)
 # print max_value
-list = [1,3,5,7,9]
-list1 = [2,4,6,8,10]
+list = [3,5,7,9,11]
+list1 = [2,6,12,8,55]
 def compare_lists(list,list1):
     for x in list:
         if x > list1[list.index(x)]:
