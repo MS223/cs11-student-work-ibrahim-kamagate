e8613c0fb3be1908217b72e843c1fb17b892f5ee
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-06 08:39:13 -0400

    Create  getting_loopy.py

---
+++ getting_loopy.py
@@ -0,0 +1,9 @@
+fruit_list = input("Give me a fruit")
+def fruit_pluralizer(list_of_strings):
+    for x in fruit_pluralizer():
+        if fruit_list[len(fruit_list)-1] == y:
+            fruit_list[:len(fruit_list-1)] + 'ies'
+            print fruit_list
+        else:
+            fruit_list[:len(fruit_list)] + 's'
+            print fruit_list
