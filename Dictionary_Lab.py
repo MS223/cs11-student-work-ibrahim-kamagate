53b975c161a9798b0d3a54b1ef3f3e8619ac73a3
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-16 08:43:09 -0400

    Create Dictionary_Lab.py

---
+++ Dictionary_Lab.py
@@ -0,0 +1,17 @@
+user_profile = {
+'name' : 'ibrahim',
+'school' : 'F&t',
+'birthday' : 'October, 14, 1999',
+'email' : 'ibrahim.kamagate@ms223.org',
+'username(s)' : 'ikamagate',
+'where are you from?' : 'South Bronx',
+'number of siblings' : '4 brothers',
+	# skills
+	# about you
+	# favorite quote
+'favorite movie' : 'Streets',
+'favorite musician' : 'Meek Mill and Chinx',
+'favorite TV show' : ''
+	# favorite book
+}
+print user_profile
