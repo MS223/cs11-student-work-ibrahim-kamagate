cd62b2b7214959d2c872982201c55c8b7ef7861d
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-23 08:42:45 -0400

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,5 +1,4 @@
-action = raw_input("What do you want today?")
-days = raw_input("What day?").lower
+# user = raw_input("How can I help you?")
 days_of_week = {
     'monday':[],
     'tuesday':[],
@@ -9,10 +8,45 @@ days_of_week = {
     'saturday':[],
     'sunday':[]
 }
+#
+# def add(user, days):
+#     days = raw_input("What day?")
+#     action = raw_input("What do you want to do?")
+#     days_of_week[days]=user.apend(days)
+#
+#
+# def get(days):
+#     print days_of_week[days]
+#     # I need an option to call choice()
+#
+# def choice():
+#     user_choice = raw_input("How can I help you?")
+#     # if they choose 'add' call add()
+#     # if they choose 'get' call get()
+# add("something", "monday")
+#
+# get("monday")
+#
+def get(days):
+    print days_of_week[days]
 
-def add():
-    days_of_week[days]=action.apend()
+
+def add(user, days):
+            days_of_week[days]=user.apend(days)
 
 
-print days_of_week
-#im trying to get the days of the week in oder
+
+def choice():
+    user_choice = raw_input("How can I help you?")
+    if user_choice == 'get':
+        day = raw_input("What day do you want to get?")
+        get(day)
+
+    elif user_choice == 'add':
+        days = raw_input("What day?")
+        action = raw_input("What do you want to do?")
+        days_of_week[days]=user_choice.apend(days)
+
+    else:
+        print "have a nice day."
+choice()
