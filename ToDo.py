94a4665d5e897f4fffc48fab324a26702896b7d2
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-24 08:27:19 -0400

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,4 +1,4 @@
-# user = raw_input("How can I help you?")
+
 days_of_week = {
     'monday':[],
     'tuesday':[],
@@ -8,45 +8,27 @@ days_of_week = {
     'saturday':[],
     'sunday':[]
 }
-#
-# def add(user, days):
-#     days = raw_input("What day?")
-#     action = raw_input("What do you want to do?")
-#     days_of_week[days]=user.apend(days)
-#
-#
-# def get(days):
-#     print days_of_week[days]
-#     # I need an option to call choice()
-#
-# def choice():
-#     user_choice = raw_input("How can I help you?")
-#     # if they choose 'add' call add()
-#     # if they choose 'get' call get()
-# add("something", "monday")
-#
-# get("monday")
-#
-def get(days):
-    print days_of_week[days]
+def get(day):
+    print days_of_week[day]
 
 
 def add(user, days):
-            days_of_week[days]=user.apend(days)
+    days_of_week[days]=user.append(days)
 
 
 
 def choice():
     user_choice = raw_input("How can I help you?")
-    if user_choice == 'get':
-        day = raw_input("What day do you want to get?")
-        get(day)
+    while user_choice != 'exit':
+        if user_choice == 'get':
+            day = raw_input("What day do you want to get?")
+            get(day)
+            user_choice = raw_input("How can I help you?")
 
-    elif user_choice == 'add':
-        days = raw_input("What day?")
-        action = raw_input("What do you want to do?")
-        days_of_week[days]=user_choice.apend(days)
+        elif user_choice == 'add':
+            days = raw_input("What day?")
+            action = raw_input("What do you want to do?")
+            days_of_week[days].append(action)
+            user_choice = raw_input("How can I help you?")
 
-    else:
-        print "have a nice day."
 choice()
