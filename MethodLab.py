ca156bc4410a07d04ca065f54135f913e66cd847
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-05-31 09:13:21 -0400

    Update MethodLab.py

--- MethodLab.py
+++ MethodLab.py
@@ -11,6 +11,7 @@ class Time(object):
         # return str(self.hour+other.hour) + ":" + str(self.minute+other.minute) + ":" + str(self.second+other.second)
 #I had to use the self and other which is time 1 and time 2
          return Time(self.hour+other.hour, self.minute+other.minute, self.second+other.second)
+#     doing it this way is better than the other one becuase it time1 and time2 and add them then it make whatever they equally to self and other is now time3
 time1 = Time(5, 32, 0)
 time2 = Time(23, 11, 11)
 time3 = Time(7, 14, 28)
